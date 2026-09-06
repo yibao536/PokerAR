17f965b2df567d0547735acd95456aae23ee4462
yibao536 <1105286540@qq.com>
2026-09-06 10:16:33 +0800

    修复 settings.gradle.kts - 配置正确的仓库

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // 用于 ARCore 和其他库
+    }
+}
+
+rootProject.name = "PokerAR"
+include(":app")
