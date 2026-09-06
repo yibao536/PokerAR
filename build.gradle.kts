d06b27bd10c9dd07e2c7b9bf24c7b2cca9f46c3c
yibao536 <1105286540@qq.com>
2026-09-06 10:19:53 +0800

    完全重写 build.gradle.kts - 添加显式仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,28 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.io.File
+
+// Top-level build file
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 task("clean", Delete::class) {
