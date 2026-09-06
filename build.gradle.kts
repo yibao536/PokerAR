1a1871fd271a7fa941ebdb3c7c1652ae626177a9
yibao536 <1105286540@qq.com>
2026-09-06 10:16:44 +0800

    完全修复根目录 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
+
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
